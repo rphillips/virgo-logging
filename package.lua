cf2428a904e8036f40ea3aefe548fbd177b805bd
Ryan Phillips <ryan.phillips@rackspace.com>
2015-01-12 10:25:33 -0600

    add package.lua

---
+++ package.lua
@@ -0,0 +1,9 @@
+return {
+  name = "virgo-logging",
+  version = "0.1.0",
+  description = "Logging Library",
+  author = "Ryan Phillips <ryan.phillips@rackspace.com>",
+  contributors = { },
+  dependencies = {
+  },
+}
