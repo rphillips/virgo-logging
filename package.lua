707cbcf305a12c816241d14d88ce11854e3cf2e7
Ryan Phillips <ryan.phillips@rackspace.com>
2015-02-06 13:00:02 -0600

    stream update

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "rphillips/logging",
-  version = "0.1.1",
+  version = "0.1.2",
   description = "Logging Library",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
   contributors = { },
