dfbcd82201d511a53a932ea78f5c5198d0e1ad7f
Ryan Phillips <ryan.phillips@rackspace.com>
2015-05-07 12:56:22 -0500

    only include lua files

--- package.lua
+++ package.lua
@@ -1,9 +1,12 @@
 return {
   name = "rphillips/logging",
-  version = "1.0.0",
+  version = "1.0.1",
   description = "Logging Library",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
   dependencies = {
     "luvit/luvit@2.1.0",
   },
+  files = {
+    "*.lua"
+  }
 }
